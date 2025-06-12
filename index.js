7538b622209fc13c6882f0294f765002a01154de
shmiki <shmiki@microsoft.com>
2025-06-12 19:08:34 +0900

    modify

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ const msalConfig = {
                 console.log(message);
             },
             piiLoggingEnabled: false,
-            logLevel: 'debug',
+            logLevel: 'verbose',
         },
     },
 };
